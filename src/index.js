764e8d0da3e70f6662dded2409bb2146a2ff2d57
audesmus <aureliu.aurelius@gmail.com>
2023-11-11 09:37:18 +0100

    site layout components created: header / menu / footer.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import { pizzaData } from "./index.js";
+// import { pizzaData } from "./index.js";
 
 import spinaci from "./pizzas/spinaci.jpg";
 
 function App() {
   return (
     <div>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -23,6 +23,28 @@ function Pizza() {
       <h1>Pizza Spinaci</h1>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
       <img src={spinaci} alt="spinaci pizza" />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>fast react pizza co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <div>
+      <p>we're currently open</p>
+      <p>current time: {new Date().toLocaleTimeString()} </p>
     </div>
   );
 }
