50e434974ff65db9b11e1c6a230ab1ced584c8ca
audesmus <aureliu.aurelius@gmail.com>
2023-11-11 09:23:43 +0100

    pizza component built + images acquired + CSS imported + pizzas data.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-export default function App() {
-  return <h1>hello worlds</h1>;
+import "./index.css";
+
+import { pizzaData } from "./index.js";
+
+import spinaci from "./pizzas/spinaci.jpg";
+
+function App() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h1>Pizza Spinaci</h1>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <img src={spinaci} alt="spinaci pizza" />
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
